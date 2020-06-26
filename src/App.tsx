eb95a14c16c087180b35d9f56e121f6f3332f1cc
Joshua Skootsky <joshua.skootsky@gmail.com>
2020-06-26 14:17:54 -0400

    feature(day): now a circle represents the whole day

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Day from './Day';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p> Day Planner goes here</p>
+       < Day />
       </header>
     </div>
   );
---
+++ src/Day.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Day.css';
+
+const Day = () => {
+    return <div className="day"> Today </div>
+}
+
+export default Day;
